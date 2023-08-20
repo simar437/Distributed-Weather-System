b072dc706a90cb5a6094e3f929244cc8ea9fe297
simar <simarjitsingharora437@gmail.com>
2023-08-20 22:37:05 +0930

    Basic implementation of sockets

--- src/AggregationServer.java
+++ src/AggregationServer.java
@@ -1,2 +1,24 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class AggregationServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(4567);
+            System.out.println("Server running...");
+            while (true) {
+                Socket s = ss.accept();
+                System.out.println("Client connected: " + s.getInetAddress());
+
+                Scanner sc = new Scanner(s.getInputStream());
+                String clientInput = sc.nextLine();
+                System.out.println("Client input: " + clientInput);
+
+                s.close(); // Close the socket after handling client interaction
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/GETClient.java
+++ src/GETClient.java
@@ -1,2 +1,16 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class GETClient {
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("localhost", 4567);
+            PrintWriter writer = new PrintWriter(s.getOutputStream(), true);
+            writer.println("Hello, World");
+        }
+        catch (Exception ignored) {
+
+        }
+    }
 }
