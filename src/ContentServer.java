29cf7a7109091515eb52289a8ad86f4ae091b29f
simar <simarjitsingharora437@gmail.com>
2023-09-05 17:56:27 +0930

    Added data

--- src/ContentServer.java
+++ src/ContentServer.java
@@ -1,3 +1,15 @@
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ContentServer {
     LamportClock localClock = new LamportClock();
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("localhost", 4567);
+
+        }
+        catch (Exception ignored) {
+
+        }
+    }
 }
