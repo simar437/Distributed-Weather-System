b58b8da93591943f47abaab082db53532dfd087c
simar <simarjitsingharora437@gmail.com>
2023-09-05 18:14:54 +0930

    Added data, read the data

--- src/ContentServer.java
+++ src/ContentServer.java
@@ -1,15 +1,18 @@
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ContentServer {
     LamportClock localClock = new LamportClock();
     public static void main(String[] args) {
         try {
             Socket s = new Socket("localhost", 4567);
-
+            String text = new String(Files.readAllBytes(Paths.get("weather data/file.txt")));
         }
-        catch (Exception ignored) {
-
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("error");
         }
     }
 }
