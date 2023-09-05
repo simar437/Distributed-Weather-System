a695553db19d61f02604be074dce7b2fa91baf25
simar <simarjitsingharora437@gmail.com>
2023-09-05 22:53:34 +0930

    Reading the contents of the file and converting it to json

--- src/ContentServer.java
+++ src/ContentServer.java
@@ -1,14 +1,33 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 public class ContentServer {
     LamportClock localClock = new LamportClock();
     public static void main(String[] args) {
         try {
-            Socket s = new Socket("localhost", 4567);
+
+            //Socket s = new Socket("localhost", 4567);
             String text = new String(Files.readAllBytes(Paths.get("weather data/file.txt")));
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, String> keyValueData = Arrays.stream(text.split("\n"))
+                    .map(line -> line.split(":", 2))
+                    .filter(parts -> parts.length == 2)
+                    .collect(Collectors.toMap(parts -> parts[0].trim(), parts -> parts[1].trim()));
+            text = objectMapper.writeValueAsString(keyValueData);
+            String request = "PUT /weather.json HTTP/1.1\n" +
+                    "User-Agent: ATOMClient/1/0\n" +
+                    "Content-Type: application/json\n" +
+                    "Content-Length: " + text.length() + "\n" +
+                    text;
+
         }
         catch (Exception e) {
             e.printStackTrace();
