d01428b8e38ae7f60877df296b0236e11a30b645
simar <simarjitsingharora437@gmail.com>
2023-09-09 12:58:35 +0930

    Weather class for handling JSON

---
+++ src/Weather.java
@@ -0,0 +1,6 @@
+public class Weather {
+    String id, name, state, time_zone,local_date_time,
+    local_date_time_full, cloud, wind_dir;
+    double lat, lon, air_temp,apparent_t, dewpt, press, rel_hum, wind_spd_kmh, wind_spd_kt;
+
+}
